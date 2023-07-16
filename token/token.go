b2c1f3513ef85f78cf903ee88f02758eb8c399e4
kcfuler <kcfuler@gmail.com>
2023-07-16 23:09:17 +0800

    feat: add type of token

--- token/token.go
+++ token/token.go
@@ -6,3 +6,30 @@ type Token struct {
 	Type    TokenType
 	Literal string
 }
+
+// 定义 token 类型
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 1343456
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
